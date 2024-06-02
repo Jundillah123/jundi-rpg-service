8ee404444797d9c1f03207e60a1b3b49a633e621
bagja akbar jaya <jundijundy255@gmail.com>
2024-06-02 23:23:27 +0700

    uptodate

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,43 @@
+
 <template>
-  <div class="about">
-    <h1>Hallo gais</h1>
+  <div class="hal1">
+    <img src="../assets/Group 522.png">
+    <ul>
+      <li link="#">Home</li> 
+      <li>About</li> 
+      <li>Serve</li> 
+      <li>Contact</li>
+    </ul>
+  </div>
+
+  <!--BAGIAN BAWAH-->
+
+  <div class="hal2">
+    <img src="https://wallpapercave.com/wp/wp2800782.jpg" >
+  </div>
+
+<!--Isi-->
+
+  <div class="box">
+    <h1><b>Moving Services</b></h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum exercitationem reprehenderit deleniti voluptate fuga nulla, minima sed quidem consequuntur ullam sequi ab optio? Autem quidem dolor nemo ullam possimus quod?</p>
+    <div class="box-k">
+      <h3><b>I Need Help</b></h3>
+      <h1><b>what the services</b></h1>
+    </div>
+  </div>
+
+  <!--BAGIAN SAMPING-->
+
+  <div class="hal3">
+    <h1><b>Lorem Ipsun</b></h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos amet totam dicta cum doloribus possimus explicabo debitis quis tenetur illo. Maxime deserunt sit sunt eius, sint dolorum ab magni nulla Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam reprehenderit commodi, asperiores obcaecati eius voluptas. Corporis quos facilis id quas?</p>
+    <div class="jun">
+      <h3><b>I Need Help</b></h3>
+      <h1><b>what the services</b></h1>
+    </div>
   </div>
 </template>
+<style>
+@import '../assets/scss/main1.scss';
+</style>
